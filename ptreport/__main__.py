81d5195c4d5316cfcfca97ec84345c8edf586da4
Jamie Hardt <jamiehardt@me.com>
2023-07-22 15:31:58 -0700

    Implemented marker display

--- ptreport/__main__.py
+++ ptreport/__main__.py
@@ -1,6 +1,7 @@
 """
 main
 """
+from itertools import chain
 
 from ptulsconv.docparser import parse_document
 from ptsl import open_engine
@@ -32,12 +33,34 @@ def main():
         sorted_clips = sorted(document.track_clips_timed(),
                               key=lambda x: x[2])
 
-        for track, clip, _, _, _ in sorted_clips:
+        sorted_clips_keyed = map(lambda x: ("Clip", x[2], x),
+                                 sorted_clips)
+
+        sorted_markers = sorted(document.markers_timed(),
+                                key=lambda x: x[1])
+
+        sorted_markers_keyed = map(lambda x: ("Marker", 
+                                              x[1] + document.header.start_time, 
+                                              x), 
+                                   sorted_markers)
+
+        sorted_events = sorted(list(sorted_clips_keyed) + list(sorted_markers_keyed),
+                               key= lambda x: x[1])
+
+        for kind, _, event in sorted_events:
             stdout.write(".XP\n")
-            stdout.write(f".B \"{clip.start_timecode} \\[->] "
-                         f"{clip.finish_timecode}\"\n")
-            stdout.write(".br\n")
-            stdout.write(f"{clip.clip_name}.\n")
+            if kind == 'Clip':
+                track = event[0]
+                clip = event[1]
+                stdout.write(f".B \"{clip.start_timecode} \\[->] "
+                             f"{clip.finish_timecode}\"\n")
+                stdout.write(".br\n")
+                stdout.write(f"{clip.clip_name}.\n")
+            elif kind == 'Marker':
+                marker = event[0]
+                stdout.write(f".B \"{marker.location} \\[DI]\"\n")
+                stdout.write(".br\n")
+                stdout.write(f"{marker.name}.\n")               
 
 
 if __name__ == "__main__":
